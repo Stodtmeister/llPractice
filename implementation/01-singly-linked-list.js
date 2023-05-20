ccf8080f4c1aecf5d58fc97d76d8ddd6f1add961
Jacob Stodtmeister <stodtmeister@gmail.com>
2023-05-20 16:42:14 -0600

    addToHead

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -22,55 +22,19 @@ class SinglyLinkedList {
 
 
     addToTail(val) {
-        const newNode = new SinglyLinkedNode(val)
 
-        if (!this.head) {
-            this.head = newNode
-        } else {
-            let current = this.head
-            while (current.next) {
-                current = current.next
-            }
-            current.next = newNode
-        }
-
-        this.length++
-        return this
     }
 
     removeFromHead() {
-        if (!this.head) return undefined
 
-        let saved = this.head
-        this.head = this.head.next
-        this.length--
-        return saved
     }
 
     removeFromTail() {
-        if (!this.head) {
-            return undefined
-        } else {
-            let current = this.head
-            let prev;
-            while (current.next) {
-                prev = current
-                current = current.next
-            }
 
-            if (!this.head.next) {
-                this.head = null
-            } else {
-                prev.next = null
-            }
-            this.length--
-            return current
-        }
     }
 
     peekAtHead() {
-        if (!this.head) return undefined
-        return this.head.value
+        
     }
 
     print() {
